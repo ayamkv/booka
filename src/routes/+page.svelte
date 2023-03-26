11ada716ded1ae326950e2f47557183ea78572b0
ayamkv <65036385+ayamkv@users.noreply.github.com>
2023-03-26 16:28:05 +0700

    add selector for kota

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,32 +12,88 @@
 	let tomorowDate = `${year}-0${month}-${day + 1}`;
 	console.log(currentDate);
 	let data = [];
+    let listKota = [];
+;
+    let kota = 'Lamongan';
     let fromCountdown
-    
+    function capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    let loading = true;
+    async function fetchData() {
+        let loading = true;
+        try {
+            const response = await fetch(`https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/${kota}/2023/03.json`);
+            data = await response.json();
+        } catch (error) {
+            console.error(error);
+        } finally {
+            loading = false;
+        }
+    }
 
-	onMount(async () => {
-		const response = await fetch(
-			'https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/lamongan/2023/03.json'
-		);
-		data = await response.json();
-	});
+    
+    onMount(async () => {
+        // Fetch the list of kota from the URL
+        const kotaResponse = await fetch(
+            'https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/kota.json'
+        );
+        listKota = await kotaResponse.json();
+
+        // Set the default kota value to the first item in the list
+        kota = listKota[128];
+
+        // Fetch the data for the selected kota
+        await fetchData();
+
+        loading = false;
+    });
+
+    function handleSelectChange() {
+        fetchData();
+    }
+	// onMount(async () => {
+	// 	const response = await fetch(
+	// 		`https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/${kota}/2023/03.json`
+	// 	);
+	// 	data = await response.json();
+        
+	// });
+    
+    $: value = kota;
 </script>
 
+<svelte:head>
+    <title>
+        ❤️ Booka - {kota}
+    </title>
+</svelte:head>
+
 <header>
-	<h1 class="text-4xl font-bold ">Maghrib dan Imsak</h1>
-	<p>Wilayah Lamongan</p>
+	<h1 class="text-4xl font-bold ">Maghrib & Imsak</h1>
+	<p class="capitalize">Wilayah {kota}</p>
     <p class="text-xs text-gray-400 italic mt-2em">nungguin ya?</p>
+ 
 </header>
 <section>
-  <div class="testdiv">
-
-  </div>
+    {#if loading}
+        <p class="text-center">loading data...</p>
+    {:else}
+    <div class="selectContainer text-center">
+        <select bind:value={kota} on:change={handleSelectChange}>
+            {#each listKota as namaKota}
+                <option value={namaKota}>
+                            {capitalize(namaKota)}
+                        </option>
+            {/each}
+            </select>
+    </div>
 	<div class="grid grid-cols-1 grid-row-2 md:gap-[-60em]">
-        <div class="flex justify-center">
+        <div class="flex flex-wrap justify-center">
             {#each data as item}
         
                 {#if item.tanggal == currentDate}
-                    <div class="card break-words whitespace-pre-line w-[40vw] min-h-[30vh] text-xl md:w-[20vw] shadow-md" id="today">
+                    <div class="card break-words whitespace-pre-line w-[50vw] min-h-[35vh] text-xl md:w-[20vw] shadow-md" id="today">
                         <div class="center">
                             <h2 class="font-bold text-md">Hari Ini</h2>
                             <p id="tanggal" class="text-sm italic">{item.tanggal}</p>
@@ -51,7 +107,7 @@
                                       <span>{remaining.minutes} :</span>
                                       <span>{remaining.seconds} </span>
                                       {:else}
-                                      <span>Selamat Beribadah Puasa</span>
+                                      <span class="text-xs">Selamat Beribadah Puasa</span>
                                       {/if}
                                     </div>
                                   </Countdown> 
@@ -75,7 +131,7 @@
                     </div>
                 {/if}
                 {#if item.tanggal == tomorowDate}
-                <div class="card break-words whitespace-pre-line w-[35vw] min-h-[30vh] text-xl md:w-[20vw] shadow-md" id="today">
+                <div class="card break-words whitespace-pre-line w-[50vw] text-xl md:w-[20vw] shadow-md" id="today">
                     <div class="center">
                         <h2 class="font-bold text-md">Besok</h2>
                         <p id="tanggal" class="text-sm italic">{item.tanggal}</p>
@@ -90,6 +146,7 @@
             {/each}
         </div>
 	</div>
+    {/if}
 </section>
 <footer>
     <div class="footer text-center m-20">
