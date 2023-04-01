36f6673d6983c402151d68955217ac38d130283c
ayamkv <65036385+ayamkv@users.noreply.github.com>
2023-04-01 18:57:01 +0700

    for now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,24 @@
     import { fade } from 'svelte/transition'
 	import { onMount } from 'svelte';
     import Countdown from '$lib/Countdown.svelte';
+
+
+
 	const date = new Date();
-
-
-
+    let tomorrow = new Date(date);
+    tomorrow.setDate(date.getDate() + 1);
     let showSpan = false;
-	let day = date.getDate();
-    // getMonth() method returns the month of a date as a zero-based index. This means that January is represented by 0.
-    // February by 1, etc. therefore  we need to add +1.
-	let month = date.getMonth() + 1; 
+	let month = date.getMonth() + 1;
+    if(month <= 9)
+        month = '0' + month;
+
+    let day= date.getDate();
+    if(day <= 9)
+        day = day.toString().padStart(2, '0');
+
+    let tomorrowDay = tomorrow.getDate().toString().padStart(2, '0');
+
+        
 	let year = date.getFullYear();
 
     console.log(month);
@@ -19,12 +28,12 @@
 
 
 	// This arrangement can be altered based on how we want the date's format to appear.
-	let currentDate = `${year}-0${month}-${day}`;
-	let tomorowDate = `${year}-0${month}-${day + 1}`;
+	let currentDate = `${year}-${month}-${day}`;
+	let tomorowDate = `${year}-${month}-${tomorrowDay}`;
 	console.log(currentDate);
+    console.log(tomorowDate)
 	let data = [];
     let listKota = [];
-;
     let kota = 'lamongan';
     let fromCountdown
     function capitalize(string) {
@@ -35,7 +44,7 @@
     async function fetchData() {
         let loading = true;
         try {
-            const response = await fetch(`https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/${kota}/${year}/0${month}.json`);
+            const response = await fetch(`https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/${kota}/${year}/${month}.json`);
             data = await response.json();
         } catch (error) {
             console.error(error);
@@ -55,13 +64,17 @@
          // Fetch the list of kota from the URL
 
         const kotaResponse = await fetch(
-            'https://gist.githubusercontent.com/ayamkv/b469d18c4a2be9832de83cb51abbd7d8/raw/04f0a9dea7096362481ade15e60dbd5e2a3b3b5c/cities.json'
+            'https://cdn.statically.io/gist/ayamkv/b469d18c4a2be9832de83cb51abbd7d8/raw/04f0a9dea7096362481ade15e60dbd5e2a3b3b5c/cities.json'
         );
         listKota = await kotaResponse.json();
+
+        
 
         // Set the default kota value to the first item in the list
         // Fetch the data for the selected kota
         await fetchData();
+
+        console.log(data)
 
         loading = false;
     });
@@ -88,7 +101,7 @@
 </svelte:head>
 
 <MetaTags 
-    title="🐪 Booka" 
+    title="Booka {capitalize(kota)}" 
     description="Waktu Maghrib & Imsyak Indonesia." 
     canonical="https://raharja.eu.org/booka"
     openGraph={{
@@ -124,18 +137,18 @@
  
 </header>
 <section class="min-h-[70vh] md:min-h-[60vh]">
-    {#if loading}
-    <div class="loader text-center items-center justify-center translate-y-48">
-        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" class="block m-auto"><rect width="10" height="10" x="1" y="1" fill="currentColor" rx="1"><animate id="svgSpinnersBlocksShuffle30" fill="freeze" attributeName="x" begin="0;svgSpinnersBlocksShuffle3b.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle31" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle38.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle32" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle39.end" dur="0.2s" values="13;1"/><animate id="svgSpinnersBlocksShuffle33" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle3a.end" dur="0.2s" values="13;1"/></rect><rect width="10" height="10" x="1" y="13" fill="currentColor" rx="1"><animate id="svgSpinnersBlocksShuffle34" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle30.end" dur="0.2s" values="13;1"/><animate id="svgSpinnersBlocksShuffle35" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle31.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle36" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle32.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle37" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle33.end" dur="0.2s" values="13;1"/></rect><rect width="10" height="10" x="13" y="13" fill="currentColor" rx="1"><animate id="svgSpinnersBlocksShuffle38" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle34.end" dur="0.2s" values="13;1"/><animate id="svgSpinnersBlocksShuffle39" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle35.end" dur="0.2s" values="13;1"/><animate id="svgSpinnersBlocksShuffle3a" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle36.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle3b" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle37.end" dur="0.2s" values="1;13"/></rect></svg>
-        <p class="mt-4">mohon bersabar...</p>
-            {#if showSpan}
-                
-                <span class="text-gray-400" in:fade="{{delay: 4000}}" >jika tidak terload, mohon <a class="underline" href="https://www.google.com/search?q=allow+javascript&oq=allow+javascript">aktifkan/izinkan Javascript pada browser anda.</a></span>
-         
-            {/if}
-        </div>
-        
+{#if loading}
+<div class="loader text-center items-center justify-center translate-y-48">
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" class="block m-auto"><rect width="10" height="10" x="1" y="1" fill="currentColor" rx="1"><animate id="svgSpinnersBlocksShuffle30" fill="freeze" attributeName="x" begin="0;svgSpinnersBlocksShuffle3b.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle31" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle38.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle32" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle39.end" dur="0.2s" values="13;1"/><animate id="svgSpinnersBlocksShuffle33" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle3a.end" dur="0.2s" values="13;1"/></rect><rect width="10" height="10" x="1" y="13" fill="currentColor" rx="1"><animate id="svgSpinnersBlocksShuffle34" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle30.end" dur="0.2s" values="13;1"/><animate id="svgSpinnersBlocksShuffle35" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle31.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle36" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle32.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle37" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle33.end" dur="0.2s" values="13;1"/></rect><rect width="10" height="10" x="13" y="13" fill="currentColor" rx="1"><animate id="svgSpinnersBlocksShuffle38" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle34.end" dur="0.2s" values="13;1"/><animate id="svgSpinnersBlocksShuffle39" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle35.end" dur="0.2s" values="13;1"/><animate id="svgSpinnersBlocksShuffle3a" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle36.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle3b" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle37.end" dur="0.2s" values="1;13"/></rect></svg>
+    <p class="mt-4">mohon bersabar...</p>
+        {#if showSpan}
+            
+            <span class="text-gray-400" in:fade="{{delay: 4000}}" >jika tidak terload, mohon <a class="underline" href="https://www.google.com/search?q=allow+javascript&oq=allow+javascript">aktifkan/izinkan Javascript pada browser anda.</a></span>
+     
+        {/if}
+    </div>
     {:else}
+
     <div class="selectContainer text-center">
         
         <select bind:value={kota} on:change={handleSelectChange} class="shadow-md inline">
@@ -151,70 +164,67 @@
             
             
     </div>
-    
-	<div class="grid grid-cols-1 grid-row-2 md:gap-[-60em]">
+        
+    <div class="grid grid-cols-1 grid-row-2 md:gap-[-60em]">
         <div class="flex flex-wrap justify-center">
             {#each data as item}
-        
+            
                 {#if item.tanggal == currentDate}
-                <a href='/{kota}/sekarang'>
-                    <div class="card break-words whitespace-pre-line w-[50vw] min-h-[35vh] text-xl md:w-[20vw] shadow-md" id="today">
-                        <div class="center">
-                            <h2 class="font-bold text-md">Hari Ini</h2>
-                            <p id="tanggal" class="text-sm italic">{item.tanggal}</p>
-                            <div class="waktu">
-                                <p id="imsyak" class="font-bold">Imsyak</p>
-                                <p class="text-4xl">{item.imsyak}</p>   
-                                <Countdown from="{item.tanggal} {item.imsyak}:00" dateFormat="YYYY-MM-DD H:m:s" let:remaining>
-                                    <div class="whatever italic text-sm" id="tanggal">
-                                      {#if remaining.done === false}
+                    <a href='/{kota}/sekarang'>
+                        <div class="card break-words whitespace-pre-line w-[50vw] min-h-[35vh] text-xl md:w-[20vw] shadow-md" id="today">
+                            <div class="center">
+                                <h2 class="font-bold text-md">Hari Ini</h2>
+                                <p id="tanggal" class="text-sm italic">{item.tanggal}</p>
+                                <div class="waktu">
+                                    <p id="imsyak" class="font-bold">Imsyak</p>
+                                    <p class="text-4xl">{item.imsyak}</p>   
+                                    <Countdown from="{item.tanggal} {item.imsyak}:00" dateFormat="YYYY-MM-DD H:m:s" let:remaining>
+                                        <div class="whatever italic text-sm" id="tanggal">
+                                        {#if remaining.done === false}
+                                            <span>{remaining.hours} :</span>
+                                            <span>{remaining.minutes} :</span>
+                                            <span>{remaining.seconds} </span>
+                                        {:else}
+                                            <span class="text-xs">Selamat Beribadah Puasa</span>
+                                        {/if}
+                                        </div>
+                                    </Countdown> 
+                                </div>
+                                <div class="waktu">
+                                    <p id="magrib" class="font-bold">Maghrib</p>
+                                    <p class="text-4xl">{item.magrib}</p>  
+                                    <Countdown from="{item.tanggal} {item.magrib}:00" dateFormat="YYYY-MM-DD H:m:s" let:remaining>
+                                        <div class="whatever italic text-sm" id="tanggal">
+                                        {#if remaining.done === false}
                                         <span>{remaining.hours} :</span>
                                         <span>{remaining.minutes} :</span>
                                         <span>{remaining.seconds} </span>
-                                      {:else}
-                                        <span class="text-xs">Selamat Beribadah Puasa</span>
-                                      {/if}
-                                    </div>
-                                  </Countdown> 
-                            </div>
-                            <div class="waktu">
-                                <p id="magrib" class="font-bold">Maghrib</p>
-                                <p class="text-4xl">{item.magrib}</p>  
-                                <Countdown from="{item.tanggal} {item.magrib}:00" dateFormat="YYYY-MM-DD H:m:s" let:remaining>
-                                    <div class="whatever italic text-sm" id="tanggal">
-                                      {#if remaining.done === false}
-                                      <span>{remaining.hours} :</span>
-                                      <span>{remaining.minutes} :</span>
-                                      <span>{remaining.seconds} </span>
-                                      {/if}
-                                    </div>
-                                  </Countdown>
-                            </div>
-                        
-                        
-                        </div>
-                    </div>
-                </a>
-                {/if}
-                {#if item.tanggal == tomorowDate}
-                <a href='/{kota}/besok'>
-                    <div class="card break-words whitespace-pre-line w-[50vw] text-xl md:w-[20vw] md:h-[35vh] shadow-md" id="today">
-                        <div class="center">
-                            <h2 class="font-bold text-md">Besok</h2>
-                            <p id="tanggal" class="text-sm italic">{item.tanggal}</p>
-                            <div class="waktu">
-                                <p id="imsyak" class="font-bold">Imsyak</p>
-                                <p class="text-4xl">{item.imsyak}</p>    
+                                        {/if}
+                                        </div>
+                                    </Countdown>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-                {/if}
-        
+                    </a>
+                    {/if}
+                    {#if item.tanggal == tomorowDate}
+                    <a href='/{kota}/besok'>
+                        <div class="card break-words whitespace-pre-line w-[50vw] text-xl md:w-[20vw] md:h-[35vh] shadow-md" id="today">
+                            <div class="center">
+                                <h2 class="font-bold text-md">Besok</h2>
+                                <p id="tanggal" class="text-sm italic">{item.tanggal}</p>
+                                <div class="waktu">
+                                    <p id="imsyak" class="font-bold">Imsyak</p>
+                                    <p class="text-4xl">{item.imsyak}</p>    
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    {/if}
             {/each}
         </div>
-	</div>
-    {/if}
+    </div>
+{/if}
 </section>
 <footer class="mt-auto">
     <div class="footer text-center">
--- src/routes/[kota]/+page.svelte
+++ src/routes/[kota]/+page.svelte
@@ -3,15 +3,25 @@
     import { MetaTags } from 'svelte-meta-tags';
 	import { onMount } from 'svelte';
     import Countdown from '$lib/Countdown.svelte';
-	const date = new Date();
-
-	let day = date.getDate();
+    const date = new Date();
+    let tomorrow = new Date(date);
+    tomorrow.setDate(date.getDate() + 1);
 	let month = date.getMonth() + 1;
+    if(month <= 9)
+        month = '0' + month;
+
+    let day= date.getDate();
+    if(day <= 9)
+        day = day.toString().padStart(2, '0');
+
+    let tomorrowDay = tomorrow.getDate().toString().padStart(2, '0');
+
+        
 	let year = date.getFullYear();
 
 	// This arrangement can be altered based on how we want the date's format to appear.
-	let currentDate = `${year}-0${month}-${day}`;
-	let tomorowDate = `${year}-0${month}-${day + 1}`;
+	let currentDate = `${year}-${month}-${day}`;
+	let tomorowDate = `${year}-${month}-${tomorrowDay}`;
 	console.log(currentDate);
     let listKota = [];
 
